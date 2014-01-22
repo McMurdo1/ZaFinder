b223452ec1003ad9dd43aab055d76de08b8db110
McMurdo1 <matt@1423ventures.com>
2014-01-22 13:58:35 -0600

    Initial Commit

---
+++ ZaFinder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZaFinder
+//
+//  Created by Matthew Graham on 1/22/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZaFinder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZaFinder
+//
+//  Created by Matthew Graham on 1/22/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
